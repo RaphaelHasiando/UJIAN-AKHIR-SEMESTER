8dd6fda0765180c161e0950f3117663e516d16e1
Raphael Hasiando <61907877+RaphaelHasiando@users.noreply.github.com>
2022-01-12 09:59:33 +0700

    Create Database.py

---
+++ Database.py
@@ -0,0 +1,10 @@
+data = {
+    'Raphael' : 90,
+    'Michael' : 80
+}
+def tambahdata(input_nama, input_nilai):
+    data[input_nama] = int(input_nilai)
+def hapusdata(input_nama):
+    del data[input_nama]
+def ubah_data():
+    print("dasdsa")
